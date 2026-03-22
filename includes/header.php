a0ecd493f354329a31829c5be189cba3cce1b7f9
mo <6061328@mborijnland.nl>
2026-03-23 00:33:03 +0100

    header

---
+++ includes/header.php
@@ -0,0 +1,26 @@
+<header class="header">
+
+  <!-- Logo -->
+  <a href="index.php" class="logo">
+    <img src="images/ChatGPT Image 9 feb 2026, 14_22_53.png" alt="Restaurant logo">
+  </a>
+
+  <!-- Hamburger menu-->
+  <button class="hamburger" id="hamburger">
+    <span></span>
+    <span></span>
+    <span></span>
+  </button>
+
+  <!-- Navigatie voor desktop -->
+  <nav class="nav">
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="lunch-diner.php">Menu</a></li>
+      <li><a href="reserveren.php">Reserveren</a></li>
+      <li><a href="vacatures.php">Vacatures</a></li>
+      <li><a href="openingstijden.php">Tijden en Locatie</a></li>
+    </ul>
+  </nav>
+
+</header>
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<title>Oma's Kost | Traditioneel Restaurant in Rotterdam</title>
+
+<meta name="description" content="Oma's Kost is een gezellig restaurant
+ in Rotterdam waar je kunt genieten van traditionele huisgemaakte gerechten, 
+ bereid met liefde en verse ingrediënten. Bezoek ons aan de Weena 6 in het 
+ centrum van Rotterdam.">
+
+<meta name="keywords" content="Oma's Kost, restaurant Rotterdam, traditioneel 
+eten, Nederlandse keuken, huisgemaakte gerechten, restaurant Weena Rotterdam, 
+eten in Rotterdam">
+
+<meta name="author" content="Mo">
+
+<link rel="stylesheet" href="style/style.css">
+<link rel="stylesheet" href="style/responsive.css">
+
+</head>
+<body>
+    <?php include 'includes/header.php' ?>
+    
+</body>
+</html>
