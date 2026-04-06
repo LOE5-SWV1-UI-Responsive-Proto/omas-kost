fd73891c07ffeb290c8cff3586ea4a01afa9d8cb
mo <6061328@mborijnland.nl>
2026-04-06 19:41:36 +0200

    css aanpassen

--- includes/footer.php
+++ includes/footer.php
@@ -1,25 +1,25 @@
 <footer class="footer">
 
-  <!-- Logo van het restaurant -->
+
   <a href="index.php" class="footer-logo">
     <img src="images/logo.png" alt="Logo van Oma's Kost">
   </a>
 
-  <!-- Contact informatie -->
+
   <section class="footer-contact">
     <h2>contact info</h2>
     <p>email: info@omaskostrotterdam.nl</p>
     <p>Telefoonnummer: 088-2344556</p>
   </section>
 
-  <!-- Social media iconen -->
+
   <section class="footer-social">
     <a href="#"><img src="images/instagram.png" alt="Instagram"></a>
     <a href="#"><img src="images/facebook logo.png" alt="Facebook"></a>
     <a href="#"><img src="images/youtube.png" alt="YouTube"></a>
   </section>
 
-  <!-- Copyright -->
+
   <section class="footer-copy">
     <p>© 2026 Oma's kost. Alle rechten voorbehouden.</p>
   </section>
